4dc65f551d41848c21390d815442fac5050c5e49
Pedro Guanaes <pedro.guanaes@gmail.com>
2024-05-16 23:47:11 +0200

    Create SignUp.js

---
+++ app/frontend/src/components/SignUp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post('/api/users/register', { name, email, password });
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUp;
