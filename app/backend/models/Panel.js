88d3f5f04cf32a39ae76a7e7cd1bae9e867d2e9e
Pedro Guanaes <pedro.guanaes@gmail.com>
2024-05-17 00:10:38 +0200

    Update Panel.js

--- app/backend/models/Panel.js
+++ app/backend/models/Panel.js
@@ -8,18 +8,11 @@ const panelSchema = new mongoose.Schema({
   },
   assigned: { type: Boolean, default: false },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  photo: { type: String },
+}, {
+  timestamps: true
 });
 
 const Panel = mongoose.model('Panel', panelSchema);
 
 module.exports = Panel;
-const panelSchema = new mongoose.Schema({
-  location: { type: String, required: true },
-  coordinates: {
-    lat: { type: Number, required: true },
-    lng: { type: Number, required: true },
-  },
-  assigned: { type: Boolean, default: false },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  photo: { type: String },
-});
