6c81c24eee498a2e13ee287281777ee576fc9638
Pedro Guanaes <pedro.guanaes@gmail.com>
2024-05-17 00:48:29 +0200

    Update App.js

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import ValidateUsers from './components/Admin/ValidateUsers';
+import Stats from './components/Admin/Stats';
+import ManageUsers from './components/Admin/ManageUsers';
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
         <Switch>
           <Route path="/signup" component={SignUp} />
           <Route path="/login" component={Login} />
+          <Route path="/admin/validate-users" component={ValidateUsers} />
+          <Route path="/admin/stats" component={Stats} />
+          <Route path="/admin/manage-users" component={ManageUsers} />
           <Route path="/" component={Dashboard} />
         </Switch>
       </div>
