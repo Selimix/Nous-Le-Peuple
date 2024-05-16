a10b7eb6d23b57ca9632abe68aadb32d8a9bf7ad
Pedro Guanaes <pedro.guanaes@gmail.com>
2024-05-16 23:47:35 +0200

    Create Login.js

---
+++ app/frontend/src/components/Login.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post('/api/users/login', { email, password });
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
